3088fa4c85759ffdf1c7890ed2cf8d12736a3d99
Paul Osman <paul@honeycomb.io>
2021-02-05 21:37:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-otel-service
+
+An example OpenTelemetry instrumented Flask application that uses an OTLP exporter to send trace data to Honeycomb.
---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+
+from flask import Flask, request
+
+from opentelemetry import propagators, trace
+from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
+
+from opentelemetry.instrumentation.wsgi import collect_request_attributes
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from opentelemetry.trace.propagation.textmap import DictGetter
+
+from grpc import ssl_channel_credentials
+
+app = Flask(__name__)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer_provider().get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="api.honeycomb.io:443",
+    insecure=False,
+    credentials=ssl_channel_credentials(),
+    headers=(
+        ("x-honeycomb-team", os.environ.get("HONEYCOMB_WRITE_KEY")),
+        ("x-honeycomb-dataset", os.environ.get("HONEYCOMB_DATASET"))
+    )
+)
+
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(otlp_exporter)
+)
+
+@app.route("/server_request")
+def server_request():
+    with tracer.start_as_current_span(
+        "server_request",
+        context=propagators.extract(DictGetter(), request.headers),
+        kind=trace.SpanKind.SERVER,
+        attributes=collect_request_attributes(request.environ)
+    ):
+        print(request.args.get("param"))
+        return "served"
+
+if __name__ == "__main__":
+    app.run(port=8082)
